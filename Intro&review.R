97ca10a835ce6e74890beeba29215e6b7c4abf71
juliannarenzi <jrenzi@email.arizona.edu>
2016-11-06 14:33:31 -0700

    D

--- Intro&review.R
+++ Intro&review.R
@@ -124,4 +124,112 @@ source("fah_to_cel_functions.R") # gets functions from a different script (e.g. 
 kel_to_cel(0)
 fah_to_kelvin(0)
 
+dat <- read.csv("data/gapminder.csv", header=TRUE)
+head(dat)
+summary(dat)
+help("str")
+str(dat)
 
+# Write a function called analyze that takes country as an argument and displays mean, min, and max lifeExp of that country
+
+#1. Subset the dataframe
+
+country_name <- subset(dat, dat$country == "Uganda")
+  
+head(country_name)
+summary(country_name)
+
+#2. Calculate mean, minimum, and max of LifeExp
+
+mean(country_name$lifeExp)
+mean(country_name[,5])  
+min(country_name$lifeExp)
+max(country_name$lifeExp)
+
+#3. The function
+
+analyze <- function(country_name) {
+  country_name <- subset(dat, dat$country == country)
+  print(mean(country_name[,5])) #use print here because otherwise will only spit out the first command
+  print(min(country_name[,5]))
+  print(max(country_name[,5]))
+}
+
+analyze("Uganda")
+
+analyze <- function(countr) {
+  country_name <- subset(dat, dat$country == countr)
+  print(mean(country_name[,5])) #use print here because otherwise will only spit out the first command
+  print(min(country_name[,5]))
+  print(max(country_name[,5]))
+  out <- c("mean_le" = mean(country_name[,5]), "min_le" = min(country_name[,5]), "max_le" = max(country_name[,5]))
+  plot(country_name$year,country_name$lifeExp, xlab="Year", ylab="Life Expectancy", main=countr)
+  print(out)
+}
+
+analyze(countr="Uganda")
+analyze("Albania")
+analyze("Cambodia")
+analyze("Senegal")
+
+# Modify the analyze function to generate a plot with year vs. LifeExp
+
+analyze("Albania")
+
+
+for (w in best_practice) {
+  print(w)
+}
+
+best_practice_fun2 <- function(some_argument) {
+  for (w in best_practice)
+    print(w)
+}
+
+best_practice_fun2(best_practice)
+
+# Generate a file that contains years "1952" and "1997" and name as gapminder_52_97
+# Another with "1997" and "2007"
+
+gapminder_52_97 <- subset(dat, dat$year == c(1952,1997))
+head(gapminder_52_97)
+
+gapminder_67_07 <- subset(dat, dat$year == 1967 | dat$year == 2007)
+head(gapminder_67_07)
+
+# Write a dataset
+
+write.csv(file = "data/gapminder_52_97.csv", gapminder_52_97, row.names = FALSE, quote = FALSE)
+write.csv(file = "data/gapminder_67_07.csv", gapminder_67_07, row.names = FALSE, quote = FALSE)
+
+# Listing the files with a pattern in a directory 
+list.files(path = "data", pattern = ".csv", full.names = TRUE)
+
+# Write a for loop to print each filename on a different line
+
+filenames <- list.files(path = "data", pattern = ".csv", full.names = TRUE)
+for (f in filenames) {
+  print(f)
+}
+
+# Using the function you wrote this morning to print out the results from multiple data-sets
+
+analyze_data <- function(file, new) {
+  file_out <- read.csv(file, header = TRUE)
+  country_name <- subset(file_out, file_out$country ==new)
+  print(mean(country_name[,5])) #use print here because otherwise will only spit out the first command
+  print(min(country_name[,5]))
+  print(max(country_name[,5]))
+  out <- c("mean_le" = mean(country_name[,5]), "min_le" = min(country_name[,5]), "max_le" = max(country_name[,5]))
+  plot(country_name$year,country_name$lifeExp, xlab="Year", ylab="Life Expectancy", main=countr)
+  print(out)
+}
+
+analyze_all <-function(pattern, new) {
+  filenames <- list.files(path = "data", pattern = ".csv", full.names = TRUE)
+  for (f in filenames) {
+    analyze_data(f,new)
+  }
+}
+
+analyze_all(".csv", "Uganda")
